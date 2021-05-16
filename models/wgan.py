c804331b37a0db676d475cf7446eb2148811b44f
Florian Hansen <flhansen.93@gmail.com>
2021-05-16 18:37:47 +0200

    Update.

--- models/wgan.py
+++ models/wgan.py
@@ -1,7 +1,6 @@
 import os
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.layers import Reshape
 from tensorflow.keras.layers import Conv2DTranspose
@@ -10,77 +9,73 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 
 class WGAN():
-    def __init__(self, path=None, latent_dim=100, gp_lambda=10):
-        if path is not None:
-            generator_path = os.path.join(path, 'generator')
-            critic_path = os.path.join(path, 'critic')
+  def __init__(self, path=None, latent_dim=100, gp_lambda=10):
+    if path is not None:
+      generator_path = os.path.join(path, 'generator')
+      critic_path = os.path.join(path, 'critic')
 
-            self.generator = tf.keras.models.load_model(generator_path, custom_objects={'generator_loss': self.generator_loss})
-            self.critic = tf.keras.models.load_model(critic_path, custom_objects={'critic_loss': self.critic_loss})
-            self.latent_dim = self.generator.input_shape[0]
-        else:
-            self.latent_dim = latent_dim
+      self.generator = tf.keras.models.load_model(generator_path, custom_objects={'generator_loss': self.generator_loss})
+      self.critic = tf.keras.models.load_model(critic_path, custom_objects={'critic_loss': self.critic_loss})
+      self.latent_dim = self.generator.input_shape[0]
+    else:
+      self.latent_dim = latent_dim
+      self.generator = self.build_generator(latent_dim)
+      self.critic = self.build_critic()
 
-            # Create the generator model
-            self.generator = self.build_generator(latent_dim)
+    self.gp_lambda = gp_lambda
 
-            # Create the critic model
-            self.critic = self.build_critic()
+  def build_generator(self, latent_dim=100):
+    model = tf.keras.Sequential()
+    model.add(Dense(7*7*256, use_bias=False, input_shape=(latent_dim,)))
 
-        self.gp_lambda = gp_lambda
+    model.add(Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)
 
-    def build_generator(self, latent_dim=100):
-        model = tf.keras.Sequential()
-        model.add(Dense(7*7*256, use_bias=False, input_shape=(latent_dim,)))
+    model.add(Conv2DTranspose(128, (5, 5), strides=1, padding='same', use_bias=False))
+    model.add(LeakyReLU())
+    assert model.output_shape == (None, 7, 7, 128)
 
-        model.add(Reshape((7, 7, 256)))
-        assert model.output_shape == (None, 7, 7, 256)
+    model.add(Conv2DTranspose(64, (5, 5), strides=2, padding='same', use_bias=False))
+    model.add(LeakyReLU())
+    assert model.output_shape == (None, 14, 14, 64)
 
-        model.add(Conv2DTranspose(128, (5, 5), strides=1, padding='same', use_bias=False))
-        model.add(LeakyReLU())
-        assert model.output_shape == (None, 7, 7, 128)
+    model.add(Conv2DTranspose(1, (5, 5), strides=2, padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
 
-        model.add(Conv2DTranspose(64, (5, 5), strides=2, padding='same', use_bias=False))
-        model.add(LeakyReLU())
-        assert model.output_shape == (None, 14, 14, 64)
+    opt = tf.keras.optimizers.Adam(1e-4, beta_1=0, beta_2=0.9)
+    model.compile(optimizer=opt, loss=self.generator_loss)
+    return model
 
-        model.add(Conv2DTranspose(1, (5, 5), strides=2, padding='same', use_bias=False, activation='tanh'))
-        assert model.output_shape == (None, 28, 28, 1)
+  def build_critic(self):
+    model = tf.keras.Sequential()
+    model.add(Conv2D(64, (5, 5), strides=2, padding='same', input_shape=[28, 28, 1]))
+    model.add(Dropout(0.3))
 
-        opt = tf.keras.optimizers.Adam(1e-4, beta_1=0, beta_2=0.9)
-        model.compile(optimizer=opt, loss=self.generator_loss)
-        return model
+    model.add(Conv2D(128, (5, 5), strides=2, padding='same'))
+    model.add(Dropout(0.3))
 
-    def build_critic(self):
-        model = tf.keras.Sequential()
-        model.add(Conv2D(64, (5, 5), strides=2, padding='same', input_shape=[28, 28, 1]))
-        model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(1))
 
-        model.add(Conv2D(128, (5, 5), strides=2, padding='same'))
-        model.add(Dropout(0.3))
+    opt = tf.keras.optimizers.Adam(1e-4, beta_1=0, beta_2=0.9)
+    model.compile(optimizer=opt, loss=self.critic_loss)
+    return model
 
-        model.add(Flatten())
-        model.add(Dense(1))
+  def gradient_penality(self, fake_sample, real_sample):
+    batch_size = fake_sample.shape[0]
+    alpha = tf.random.uniform([batch_size, 1, 1, 1], 0.0, 1.0)
+    inter_sample = fake_sample * alpha + real_sample * (1 - alpha)
 
-        opt = tf.keras.optimizers.Adam(1e-4, beta_1=0, beta_2=0.9)
-        model.compile(optimizer=opt, loss=self.critic_loss)
-        return model
+    with tf.GradientTape() as tape_gp:
+        tape_gp.watch(inter_sample)
+        inter_score = self.critic(inter_sample)
 
-    def gradient_penality(self, fake_sample, real_sample):
-        batch_size = fake_sample.shape[0]
-        alpha = tf.random.uniform([batch_size, 1, 1, 1], 0.0, 1.0)
-        inter_sample = fake_sample * alpha + real_sample * (1 - alpha)
+    gp_gradients = tape_gp.gradient(inter_score, inter_sample)
+    gp_gradients_norm = tf.sqrt(tf.reduce_sum(tf.square(gp_gradients), axis=[1, 2, 3]))
+    return tf.reduce_mean((gp_gradients_norm - 1.0) ** 2)
 
-        with tf.GradientTape() as tape_gp:
-            tape_gp.watch(inter_sample)
-            inter_score = self.critic(inter_sample)
+  def generator_loss(self, fake_score):
+    return -tf.reduce_mean(fake_score)
 
-        gp_gradients = tape_gp.gradient(inter_score, inter_sample)
-        gp_gradients_norm = tf.sqrt(tf.reduce_sum(tf.square(gp_gradients), axis=[1, 2, 3]))
-        return tf.reduce_mean((gp_gradients_norm - 1.0) ** 2)
-
-    def generator_loss(self, fake_score):
-        return -tf.reduce_mean(fake_score)
-
-    def critic_loss(self, fake_score, real_score, gp):
-        return tf.reduce_mean(fake_score) - tf.reduce_mean(real_score) + gp * self.gp_lambda
+  def critic_loss(self, fake_score, real_score, gp):
+    return tf.reduce_mean(fake_score) - tf.reduce_mean(real_score) + gp * self.gp_lambda
--- train.py
+++ train.py
@@ -1,35 +1,79 @@
 from argparse import ArgumentParser
+from models.wgan import WGAN
+import tensorflow as tf
 
-def train_step_generator(generator_model):
-    return None
+@tf.function
+def train_step_generator(gan: WGAN, batch_size):
+  noise = tf.random.uniform([batch_size, gan.latent_dim])
 
-def train_step_critic(critic_model):
-    return None
+  with tf.GradientTape() as tape:
+    fake_samples = gan.generator(noise, training=True)
+    fake_score = gan.critic(fake_samples, training=True)
+    loss = gan.generator_loss(fake_score)
 
-def train(generator_model, critic_model, n_critic):
-    # Train the critic more than the generator
-    for _ in range(n_critic):
-        train_step_critic(critic_model)
+  gradient = tape.gradient(loss, gan.generator.trainable_variables)
+  gan.generator.optimizer.apply_gradients(zip(gradient, gan.generator.trainable_variables))
+  return loss
 
-    # Train the generator model
-    train_step_generator(generator_model)
+@tf.function
+def train_step_critic(gan: WGAN, real_samples):
+  noise = tf.random.uniform([real_samples.shape[0], gan.latent_dim])
+
+  with tf.GradientTape() as tape:
+    fake_sample = gan.generator(noise)
+    real_sample = real_samples
+
+    fake_score = gan.critic(fake_sample)
+    real_score = gan.critic(real_sample)
+
+    gp = gan.gradient_penality(fake_sample, real_sample)
+    loss = gan.critic_loss(fake_score, real_score, gp)
+
+  gradient = tape.gradient(loss, gan.critic.trainable_variables)
+  gan.critic.optimizer.apply_gradients(zip(gradient, gan.critic.trainable_variables))
+  return loss
+
+def train(gan: WGAN, dataset, epochs, batch_size, n_critic):
+  generator_loss = tf.keras.metrics.Mean()
+  critic_loss = tf.keras.metrics.Mean()
+
+  for epoch in range(epochs):
+    for batch in dataset:
+      for _ in range(n_critic):
+        d_loss = train_step_critic(gan, batch)
+        critic_loss(d_loss)
+
+      g_loss = train_step_generator(gan, batch_size)
+      generator_loss(g_loss)
+
+    print(f'epoch: {epoch}, g_loss: {generator_loss.result():6.3f}, d_loss: {critic_loss.result():6.3f}')
+    generator_loss.reset_states()
+    critic_loss.reset_states()
+
+def read_dataset(buffer_size, batch_size):
+  (x_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
+  x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
+  x_train = (x_train - 127.5) / 127.5
+  train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(buffer_size).batch(batch_size)
+  return train_dataset
 
 def main(args):
-    train(None, None, args.n_critic)
+  wgan = WGAN()
+  dataset = read_dataset(60000, args.batch_size)
+  train(wgan, dataset, args.epochs, args.batch_size, args.n_critic)
 
 if __name__ == "__main__":
-    # Define arguments of the script
-    parser = ArgumentParser()
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--save-interval', type=int, default=100)
-    parser.add_argument('--batch-size', type=int, default=25)
-    parser.add_argument('--generator-learning-rate', type=float, default=1e-4)
-    parser.add_argument('--discriminator-learning-rate', type=float, default=1e-4)
-    parser.add_argument('--n-critic', type=int, default=5)
-    parser.add_argument('--dataset-dir', type=str, default='dataset')
+  # Define arguments of the script
+  parser = ArgumentParser()
+  parser.add_argument('--epochs', type=int, default=100)
+  parser.add_argument('--save-interval', type=int, default=100)
+  parser.add_argument('--batch-size', type=int, default=32)
+  parser.add_argument('--generator-learning-rate', type=float, default=1e-4)
+  parser.add_argument('--discriminator-learning-rate', type=float, default=2e-4)
+  parser.add_argument('--n-critic', type=int, default=5)
+  parser.add_argument('--dataset-dir', type=str, default='dataset')
 
-    # Parse arguments from command line
-    args = parser.parse_args()
-
-    # Call the main function with arguments from command line
-    main(args)
+  # Parse arguments from command line
+  args = parser.parse_args()
+  # Call the main function with arguments from command line
+  main(args)
